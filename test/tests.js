41d4445328145a166de073037e2fabe4be0519eb
Fogus <mefogus@gmail.com>
2020-04-14 15:28:03 -0400

    more literal tests

--- test/tests.js
+++ test/tests.js
@@ -39,7 +39,8 @@ QUnit.test( "read forms", function(assert) {
   assert.deepEqual(lisp.read("()"), [], "empty array");
   assert.deepEqual(lisp.read("(a)"), ["'a"], "array");
   assert.deepEqual(lisp.read("(+ 1 2)"), ["'+", 1, 2], "array");
-  assert.deepEqual(lisp.read("(λ (a b) (+ a b))"), ["'λ", ["'a", "'b"], ["'+", "'a", "'b"]], "function form");
+  assert.deepEqual(lisp.read("(λ (a b) (+ a b))"), ["'λ", ["'a", "'b"], ["'+", "'a", "'b"]], "lambda");
+  assert.deepEqual(lisp.read("{|a b| (+ a b)}"), ["'λ", {0 : "'a", 1: "'b"}, ["'+", "'a", "'b"]], "function literal form");  
   assert.deepEqual(lisp.read("'a"), ["'quote", "'a"], "quoted symbol");
   assert.deepEqual(lisp.read("'(a)"), ["'quote", ["'a"]], "quoted array");
   assert.deepEqual(lisp.read("'(a b (c (d)))"), ["'quote", ["'a", "'b", ["'c", ["'d"]]]], "quoted nested array");
