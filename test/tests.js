3b1bcc8d07146806f29b4c96b09e843bee6906d9
Fogus <mefogus@gmail.com>
2020-04-14 12:05:14 -0400

    tests for quote and quotations

--- test/tests.js
+++ test/tests.js
@@ -35,10 +35,13 @@ QUnit.test( "thunks", function(assert) {
 
 QUnit.test( "read forms", function(assert) {
   assert.deepEqual(lisp.read("a"), "'a", "symbols");
-  assert.deepEqual(lisp.read("|"), "'|", "symbols");
+  assert.deepEqual(lisp.read("|a|"), {0 : "'a"}, "quotations");
   assert.deepEqual(lisp.read("()"), [], "empty array");
   assert.deepEqual(lisp.read("(a)"), ["'a"], "array");
   assert.deepEqual(lisp.read("(+ 1 2)"), ["'+", 1, 2], "array");
   assert.deepEqual(lisp.read("(λ (a b) (+ a b))"), ["'λ", ["'a", "'b"], ["'+", "'a", "'b"]], "function form");
+  assert.deepEqual(lisp.read("'a"), ["'quote", "'a"], "quoted symbol");
+  assert.deepEqual(lisp.read("'(a)"), ["'quote", ["'a"]], "quoted array");
+  assert.deepEqual(lisp.read("'(a b (c (d)))"), ["'quote", ["'a", "'b", ["'c", ["'d"]]]], "quoted nested array");
 });
 
