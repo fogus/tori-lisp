8e9b4b72525fa0f87561a07b61ed074143cf5826
Fogus <mefogus@gmail.com>
2020-06-22 16:09:27 -0400

    version

--- repl.js
+++ repl.js
@@ -1,7 +1,7 @@
 const repl = require("repl");
 var 鳥 = require("./lisp").lisp;
 
-console.log("Starting tori-lisp version " + 鳥.VERSION + "...");
+console.log("Starting tori-lisp version " + 鳥.VERSION + "...\n");
 
 repl.start({
   prompt: "鳥 ",
