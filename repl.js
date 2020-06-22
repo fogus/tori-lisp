00a81eba8bd86ebd71c0160a6f5550622bec25c8
Fogus <mefogus@gmail.com>
2020-06-22 15:42:05 -0400

    tori

--- repl.js
+++ repl.js
@@ -3,9 +3,10 @@ var 鳥 = require("./lisp").lisp;
 
 repl.start({
   prompt: "鳥 ",
-  eval: function(cmd, context, filename, callback) {
-    var ret = 鳥.evil(L.core, 鳥.read(cmd));
-    callback(null, ret);
+    eval: function(cmd, context, filename, callback) {
+	console.log("Starting the REPL");
+	var ret = 鳥.evil(L.core, 鳥.read(cmd));
+	callback(null, ret);
   }
 });
 
