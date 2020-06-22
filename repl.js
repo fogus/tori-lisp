e8c05b99d201dc0bc7d7137795747221cd8769cb
Fogus <mefogus@gmail.com>
2020-06-22 16:16:44 -0400

    Stub to load infiles

--- repl.js
+++ repl.js
@@ -1,7 +1,9 @@
 const repl = require("repl");
 var 鳥 = require("./lisp").lisp;
 
-console.log("Starting tori-lisp version " + 鳥.VERSION + "...\n");
+console.log("Starting tori-lisp v" + 鳥.VERSION + "...");
+process.argv.slice(2).forEach(infile => console.log("...loading " + infile));
+console.log("done\n");
 
 repl.start({
   prompt: "鳥 ",
