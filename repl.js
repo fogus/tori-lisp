4e3ec36b7a1a46ea5f7a41cadb0780ebe0985b49
Fogus <mefogus@gmail.com>
2020-06-23 11:24:48 -0400

    load file to string'

--- repl.js
+++ repl.js
@@ -1,8 +1,14 @@
 const repl = require("repl");
+const fs = require('fs');
+
 var 鳥 = require("./lisp").lisp;
 
 console.log("Starting tori-lisp v" + 鳥.VERSION + "...");
-process.argv.slice(2).forEach(infile => console.log("...loading " + infile));
+process.argv.slice(2).forEach(function(infile) {
+    var src = fs.readFileSync(infile, "utf8");
+    console.log("...loading " + infile);
+    console.log(src);
+});
 console.log("done\n");
 
 repl.start({
