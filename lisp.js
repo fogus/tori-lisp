dc50544a34e11db58fd489db707ff013d4f466da
Fogus <mefogus@gmail.com>
2020-06-25 14:36:27 -0400

    autocurry

--- lisp.js
+++ lisp.js
@@ -26,6 +26,35 @@
     }
   }
 
+  var autoCurry = (function () {
+    var toArray = function toArray(arr, from) {
+      return Array.prototype.slice.call(arr, from || 0);
+    };
+	
+    var curry = function curry(fn /* variadic number of args */) {
+      var args = toArray(arguments, 1);
+      return function curried() {
+        return fn.apply(this, args.concat(toArray(arguments)));
+      };
+    };
+    
+    return function autoCurry(fn, numArgs) {
+      numArgs = numArgs || fn.length;
+      return function autoCurried() {
+        if (arguments.length < numArgs) {
+          return numArgs - arguments.length > 0 ?
+            autoCurry(curry.apply(this, [fn].concat(toArray(arguments))),
+                      numArgs - arguments.length) :
+            curry.apply(this, [fn].concat(toArray(arguments)));
+        }
+        else {
+          return fn.apply(this, arguments);
+        }
+      };
+    };
+    
+  }());
+  
   var part = function(n, array) {
     var i, j;
     var res = [];
