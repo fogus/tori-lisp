a0c85ebc5e7c86d8f0bc41c0c1951b746821ad39
Fogus <mefogus@gmail.com>
2020-08-16 10:14:04 -0400

    Matching to the tut.txt

--- repl.js
+++ repl.js
@@ -1,7 +1,11 @@
-const repl = require("repl");
+const repl = require('repl');
 const fs = require('fs');
+const util = require('util');
 
 var 鳥 = require("./lisp").lisp;
+
+const writer = (obj) => ";;=> " + util.inspect(obj, writer.options);
+writer.options = { ...util.inspect.defaultOptions, showProxy: true, colors: true };
 
 console.log("Starting tori-lisp v" + 鳥.VERSION + "...");
 process.argv.slice(2).forEach(function(infile) {
@@ -12,10 +16,11 @@ process.argv.slice(2).forEach(function(infile) {
 console.log("done\n");
 
 repl.start({
-  prompt: "鳥 ",
+  prompt: "鳥>  ",
   eval: function(cmd, context, filename, callback) {
     var ret = 鳥.evil(鳥.core, 鳥.read(cmd));
     callback(null, ret);
-  }
+  },
+  writer: writer
 });
 
