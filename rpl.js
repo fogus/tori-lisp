53bf9b411c89445372215b52fb5b401be3e26bd5
Fogus <mefogus@gmail.com>
2020-08-27 15:42:26 -0400

    added read, print, loop to test reading and tokenizing

---
+++ rpl.js
@@ -0,0 +1,20 @@
+const repl = require('repl');
+const fs = require('fs');
+const util = require('util');
+
+var 鳥 = require("./lisp").lisp;
+
+const writer = (obj) => ";;=> " + util.inspect(obj, writer.options);
+writer.options = { ...util.inspect.defaultOptions, showProxy: true, colors: true };
+
+console.log("Starting tori-lisp reader v" + 鳥.VERSION + "...");
+
+repl.start({
+  prompt: "鳥r>  ",
+  eval: function(cmd, context, filename, callback) {
+    var ret = 鳥.tokenize(cmd, { word:/\w+/, whitespace:/\s+/, punctuation:/[^\w\s]/ }, 'invalid');
+    callback(null, ret);
+  },
+  writer: writer
+});
+
