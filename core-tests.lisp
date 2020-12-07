9c3a640ff9d8b3e0f551c103e26748273058f14f
Fogus <mefogus@gmail.com>
2020-12-07 12:01:06 -0500

    tests for filter / remove

--- core-tests.lisp
+++ core-tests.lisp
@@ -22,3 +22,6 @@
 (check {(map (+ 10) '(1 2 3))} eqv? [ 11, 12, 13 ] "Testing that a curried function works with map.")
 (check {(map (comp first rest) '((a b) (c d) (e f)))} eqv? [ 'b 'd 'f ] "Testing that a composed function works with map.")
 (check {(map (comp not odd?) '(1 2 3 4 5 6))} eqv? [ #f #t #f #t #f #t ] "Testing that a composed function works with map and not.")
+
+(check {(filter odd? '(1 2 3 4 5 6 7))} eqv? [1 3 5 7] "Testing filter.")
+(check {(remove odd? '(1 2 3 4 5 6 7))} eqv? [2 4 6] "Testing remove.")
