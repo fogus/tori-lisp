ffc359985e10cdbff7d6db85ee096f8bf454afd3
Fogus <mefogus@gmail.com>
2020-12-07 11:48:49 -0500

    mylen tests

--- core-tests.lisp
+++ core-tests.lisp
@@ -7,3 +7,13 @@
 (check {(mylen nil)}      is? 0 "Testing that mylen of nil is 0")
 (check {(mylen '(a b c))} is? 3 "Testing that mylen of a list is 3")
 (check {(mylen [1 2 3])}  is? 3 "Testing that mylen of a list literal is 3")
+
+
+(def translate 
+  (λ (sym)
+     (cond
+       (is? sym 'apple) 'mela 
+       (is? sym 'onion) 'cipolla
+       #t 'che?)))
+
+(check {(translate 'apple)} is? 'mela "Testing that translate works.")
