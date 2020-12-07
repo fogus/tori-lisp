0964d90c8fc7fc1e30f0012962ba2af11f406473
Fogus <mefogus@gmail.com>
2020-12-07 11:47:27 -0500

    mylen tests

--- core-tests.lisp
+++ core-tests.lisp
@@ -4,4 +4,6 @@
        0
        (+ 1 (mylen (rest xs))))))
 
-(check {(mylen nil)} is? 1 "Testing that mylen of nil is 0")
+(check {(mylen nil)}      is? 0 "Testing that mylen of nil is 0")
+(check {(mylen '(a b c))} is? 3 "Testing that mylen of a list is 3")
+(check {(mylen [1 2 3])}  is? 3 "Testing that mylen of a list literal is 3")
